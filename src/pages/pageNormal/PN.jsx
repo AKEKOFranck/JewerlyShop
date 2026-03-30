df74d577785f20fbb847d159ac86e7106cd3c284
AKEKOFranck <142504737+MauriceAkeko@users.noreply.github.com>
2026-03-30 23:57:09 +0000

    maj : new page

--- src/pages/pageNormal/PN.jsx
+++ src/pages/pageNormal/PN.jsx
@@ -1,34 +1,165 @@
 import '../../styles/stylePN/PN.css';
 import { Link } from 'react-router-dom';
 import pic1 from '../../assets/Ebague.png';
+import pic2 from '../../assets/Eboucles.png';
 export default function PN() {
+    const dataProducts = [
+        {
+            id:1 ,
+            details: [
+                "name",
+                "prix",
+                "taille",
+                "type",
+            ],
+            disponibility:" dispo",
+             image: pic1,
+        },
+
+          {
+            id:2 ,
+            details: [
+                "name",
+                "prix",
+                "taille",
+                "type",
+            ],
+            disponibility:" dispo",
+             image: pic2,
+        }
+    ]
+
     return(
         <main>
 
-            <section className='pn_header'>
-
-            </section>
+            <section className='pn_header'></section>
 
            <section className='pn_cont'>
-            <div className='pn_card'>
-                <img src={pic1} alt="Ebague" className='pn_card_img'/>
-                <div className='pn_card_text'>
-                    <p className='pn_card_n'>Nome</p>
-                    <p className='pn_card_p'> Price </p>
-                    <p className='pn_card_t'> type </p>
-                    <p className='pn_card_qt'> Disponible </p>
+
+                 <div className='pn_box'>
+                    
+{dataProducts.map(data => (
+    <div className='pn_card' key={data.id}>
+        <img src={data.image} alt={data.name} className='pn_img'/>
+        <div className='pn_items'>
+            <ul>
+                {data.details.map((detail, index) => (
+                    <li key={index}>{detail}</li>
+                ))}
+            </ul>
+            <p className='pn_qt'>{data.disponibility}</p>
+            <button className='pn_bt'>commander</button>
+        </div>
+    </div>
+))}
+
+
+                 </div>
+
+                <div className='pn_box'>
+                    
+{dataProducts.map(data => (
+    <div className='pn_card' key={data.id}>
+        <img src={data.image} alt={data.name} className='pn_img'/>
+        <div className='pn_items'>
+            <ul>
+                {data.details.map((detail, index) => (
+                    <li key={index}>{detail}</li>
+                ))}
+            </ul>
+            <p className='pn_qt'>{data.disponibility}</p>
+            <button className='pn_bt'>commander</button>
+        </div>
+    </div>
+))}
                 </div>
-                <button className='pn_card_btn'> Commander </button>
-            </div>
+                    
            </section>
 
            <section className='pn_cont'>
-            
+
+                <div className='pn_box'>
+                    
+{dataProducts.map(data => (
+    <div className='pn_card' key={data.id}>
+        <img src={data.image} alt={data.name} className='pn_img'/>
+        <div className='pn_items'>
+            <ul>
+                {data.details.map((detail, index) => (
+                    <li key={index}>{detail}</li>
+                ))}
+            </ul>
+            <p className='pn_qt'>{data.disponibility}</p>
+            <button className='pn_bt'>commander</button>
+        </div>
+    </div>
+))}
+                </div>
+
+                <div className='pn_box'>
+                    
+{dataProducts.map(data => (
+    <div className='pn_card' key={data.id}>
+        <img src={data.image} alt={data.name} className='pn_img'/>
+        <div className='pn_items'>
+            <ul>
+                {data.details.map((detail, index) => (
+                    <li key={index}>{detail}</li>
+                ))}
+            </ul>
+            <p className='pn_qt'>{data.disponibility}</p>
+            <button className='pn_bt'>commander</button>
+        </div>
+    </div>
+))}
+                </div>
+
            </section>
 
            <section className='pn_cont'>
-            
+
+                <div className='pn_box'>
+                    
+{dataProducts.map(data => (
+    <div className='pn_card' key={data.id}>
+        <img src={data.image} alt={data.name} className='pn_img'/>
+        <div className='pn_items'>
+            <ul>
+                {data.details.map((detail, index) => (
+                    <li key={index}>{detail}</li>
+                ))}
+            </ul>
+            <p className='pn_qt'>{data.disponibility}</p>
+            <button className='pn_bt'>commander</button>
+        </div>
+    </div>
+))}
+                </div>
+
+                <div className='pn_box'>
+                    
+{dataProducts.map(data => (
+    <div className='pn_card' key={data.id}>
+        <img src={data.image} alt={data.name} className='pn_img'/>
+        <div className='pn_items'>
+            <ul>
+                {data.details.map((detail, index) => (
+                    <li key={index}>{detail}</li>
+                ))}
+            </ul>
+            <p className='pn_qt'>{data.disponibility}</p>
+            <button className='pn_bt'>commander</button>
+        </div>
+    </div>
+))}
+                </div>
+
            </section>
         </main>
     )
 }
+
+
+
+
+  
